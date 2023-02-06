542ead0a88dfaa0e8f566266ce69a2ec04c52fe6
Khwanchanok-S <khwanchanoksriphattharasith@gmail.com>
2023-02-06 11:54:35 +0700

    comiit

--- src/app.js
+++ src/app.js
@@ -1,2 +1,21 @@
-// const { sequelize } = require('./models');
-// sequelize.sync({ force: true });dit
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
+
+const notFoundMiddleware = require('./middlewares/not-found');
+const errorMiddleware = require('./middlewares/error');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () =>
+  console.log(chalk.yellowBright.bold(`server running on port: ${port}`)),
+);
